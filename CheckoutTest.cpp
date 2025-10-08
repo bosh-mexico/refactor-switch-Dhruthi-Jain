138cfe32b3a1ee40b36e92ca2e0c1f66cde57f6a
Dhruthi-Jain <83956221+Dhruthi-Jain@users.noreply.github.com>
2025-10-08 15:39:53 +0530

    Update CheckoutTest.cpp

--- CheckoutTest.cpp
+++ CheckoutTest.cpp
@@ -1,1 +1,20 @@
+#include "PaymentSystem.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
+class MockPayment {
+public:
+    MOCK_METHOD(void, payPal, (double), ());
+    MOCK_METHOD(void, googlePay, (double), ());
+    MOCK_METHOD(void, creditCard, (double), ());
+};
+
+void checkoutWithMock(PaymentMode mode, double amount, MockPayment& mock) {
+    if (mode == PaymentMode::PAYPAL) {
+        mock.payPal(amount);
+    } else if (mode == PaymentMode::GOOGLEPAY) {
+        mock.googlePay(amount);
+    } else if (mode == PaymentMode::CREDITCARD) {
+        mock.creditCard(amount);
+    }
+}
